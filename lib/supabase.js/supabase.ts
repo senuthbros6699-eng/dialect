759104f7a965011d689c1c5334cfb32816bfd64b
senuth <senuthbros@gmail.com>
2025-11-23 11:16:18 +0530

    Fix supabase connection

--- lib/supabase.js/supabase.ts
+++ lib/supabase.js/supabase.ts
@@ -1,9 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// Notice the quotes '' around the URL below
-const supabaseUrl = 'https://nknmmrzrzyfgintyeqpd.supabase.co'
+// This tells the app to look for the keys in Netlify/Vercel settings
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Notice the quotes '' around the Key below
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbm1tcnpyenlmZ2ludHllcXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4Mjg0MjUsImV4cCI6MjA3OTQwNDQyNX0.R6OLqZ-sfqeBUeXr8rUKbHvqNH80_Qnuss3K-MuUx0M'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
